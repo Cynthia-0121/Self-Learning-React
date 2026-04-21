13d412c5c7d5db7e8568f9e73f79f26e2f710a37
Cynth1a <cynthiatan0121@gmail.com>
2026-04-21 22:59:59 +0800

    List

--- EP 06/src/List.jsx
+++ EP 06/src/List.jsx
@@ -9,7 +9,8 @@ function List () {
     const fruits = [{id: "F001", name: "watermelon", calories: 95}, 
                     {id: "F002", name: "apple", calories: 45}, 
                     {id: "F003", name: "grape", calories: 105}, 
-                    {id: "F004", name: "cherry", calories: 37}];
+                    {id: "F004", name: "cherry", calories: 37},
+                    {id: "F005", name: "banana", calories: 67}];
 
     const listItems = fruits.map(fruit => <li key = {fruit.id}>{fruit.name}</li>);
     return(<ol>{listItems}</ol>);
